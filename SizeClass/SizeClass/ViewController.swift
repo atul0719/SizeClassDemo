48a5e793ed18bf9cc89e39864cf5e60b4182a958
Atul <Atul.depex@gmail.com>
2019-09-24 11:16:51 +0530

    check for size class

---
+++ SizeClass/SizeClass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SizeClass
+//
+//  Created by Depex Technologies Pvt. Ltd on 02/04/19.
+//  Copyright © 2019 Depex Technologies Pvt. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
